23ff50f288ddd181120d987f8ff3ad7fa861ed5b
Jason Bacchetta <jabacchetta@gmail.com>
2016-06-01 01:26:56 -0500

    Modularize webpack config
    Refactored code to make it easier to maintain in the future.

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,70 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const WebpackBrowserPlugin = require('webpack-browser-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const target = process.env.npm_lifecycle_event;
 const source = `${__dirname}/client/source/`;
-const autoprefixer = require('autoprefixer');
-const merge = require('webpack-merge');
+const command = process.env.npm_lifecycle_event;
 const cssnano = require('cssnano');
 const webpack = require('webpack');
-const config = {
-  entry: source,
-  output: {
-    path: `${__dirname}/client/build/`,
-    filename: 'bundle.js',
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        include: source,
-        loaders: ['babel?presets[]=es2015,presets[]=react'],
-      },
-      {
-        test: /\.css$/,
-        include: source,
-        loader: ExtractTextPlugin.extract('isomorphic-style', 'css', 'postcss'),
-      }
-    ],
-  },
-  postcss: function () {
-    return [autoprefixer, cssnano];
-  },
-  plugins: [
-    new WebpackBrowserPlugin(),
-    new HtmlWebpackPlugin({
-      template: `${source}index.html`,
-    }),
-    new ExtractTextPlugin("style.css", {
-        allChunks: false,
-      }
-    )
-  ],
-};
-
-if (target === 'build') {
-  config.module.loaders[0].loaders.push('eslint');
-  config.output.filename = 'bundle.min.js';
-  config.plugins.push(
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      }
-    })
-  );
-  config.plugins[1].options.minify = {
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackBrowserPlugin = require('webpack-browser-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const loadBrowse = new WebpackBrowserPlugin();
+const removeDup = new webpack.optimize.DedupePlugin();
+const extractCSS = new ExtractTextPlugin("style.min.css");
+const minJS = new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false, }, });
+const addHTML = new HtmlWebpackPlugin({
+  template: `${source}index.html`,
+  minify: {
     collapseWhitespace: true,
     collapseInlineTagWhitespace: true,
     html5: true,
     removeAttributeQuotes: true,
     removeComments: true,
     removeRedundantAttributes: true,
-  };
-}
+  },
+});
 
-module.exports = config;
+// Configuration settings according to npm command used
+const settings = {
+  // npm run start
+  start: {
+    filename: 'bundle.js',
+    loaders: {
+      js: ['babel?presets[]=es2015,presets[]=react'],
+      css: ExtractTextPlugin.extract('isomorphic-style', 'css', 'postcss'),
+    },
+    plugins: [loadBrowse, extractCSS, addHTML],
+  },
+
+  // npm run build
+  build: {
+    filename: 'bundle.min.js',
+    loaders: {
+      js: ['babel?presets[]=es2015,presets[]=react', 'eslint'],
+      css: ExtractTextPlugin.extract('isomorphic-style', 'css', 'postcss'),
+    },
+    plugins: [loadBrowse, extractCSS, addHTML, removeDup, minJS],
+  },
+};
+
+module.exports = {
+  entry: source,
+  output: {
+    path: `${__dirname}/client/build/`,
+    filename: settings[command].filename,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: source,
+        loaders: settings[command].loaders.js,
+      },
+      {
+        test: /\.css$/,
+        include: source,
+        loader: settings[command].loaders.css,
+      }
+    ],
+  },
+  postcss: [autoprefixer, cssnano],
+  plugins: settings[command].plugins,
+};
