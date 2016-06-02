843c39f8ac34265852778afaf6e3d0ef1a4a1cc4
cruz02148 <cruz02148@gmail.com>
2016-06-02 13:50:39 -0400

    Began to implement the table for the workout view/component

--- client/source/index.js
+++ client/source/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './scripts/components/app.jsx';
+import Workout from './scripts/components/Workout.jsx';
 require('./styles/style.css');
 
-ReactDOM.render(<App />, document.getElementById('app'));
+//ReactDOM.render(<App />, document.getElementById('app'));
+
+// This is the new component for the Workout. This will need to be modified to use React-Router
+ReactDOM.render(<Workout />, document.getElementById('app'));
