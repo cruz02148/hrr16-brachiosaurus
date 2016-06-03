ba1aefef6d15ffcde4ad5e92e80b99703e0d7082
Jason Bacchetta <jabacchetta@gmail.com>
2016-06-02 21:39:35 -0500

    Prepare for upstream pull
    Adding files in preparation for upstream pull.

--- client/source/index.js
+++ client/source/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './scripts/components/app.jsx';
+import Header from './scripts/components/header.jsx';
 require('./styles/style.css');
+import { Router, Route, Link, browserHistory } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="about" component={Header}/>
+    </Route>
+  </Router>
+), document.getElementById('app'));
--- server/db/models/firebase.js
+++ server/db/models/firebase.js
@@ -1,3 +1,10 @@
-database.once('value').then(function (snapshot) {
-  var username = snapshot.val().username;
-});
+// database.once('value').then(function (snapshot) {
+//   var username = snapshot.val().username;
+// });
+var database = firebase.database();
+const workouts = database.ref('workouts/easy');
+
+// workouts.once("value", function(data) {
+//   console.log(data);
+// });
+console.log(workouts);
